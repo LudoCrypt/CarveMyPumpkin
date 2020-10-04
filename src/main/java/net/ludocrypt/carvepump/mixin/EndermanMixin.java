b014c2352107d7e9fec460f79e36391ec587c0b2
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2020-10-04 13:46:24 -0500

    I am a terrible guesser

--- src/main/java/net/ludocrypt/carvepump/mixin/EndermanMixin.java
+++ src/main/java/net/ludocrypt/carvepump/mixin/EndermanMixin.java
@@ -5,10 +5,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.ludocrypt.carvepump.CarveMyPumpkin;
-import net.minecraft.block.Blocks;
+import net.ludocrypt.carvepump.blocks.CarvableBlock;
 import net.minecraft.entity.mob.EndermanEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 
 @Mixin(EndermanEntity.class)
@@ -17,8 +17,8 @@ public class EndermanMixin {
 	@Inject(method = "isPlayerStaring", at = @At("HEAD"), cancellable = true)
 	private void isPlayerStaring(PlayerEntity player, CallbackInfoReturnable<Boolean> ci) {
 		ItemStack itemStack = (ItemStack) player.inventory.armor.get(3);
-		if (itemStack.getItem() == Blocks.CARVED_PUMPKIN.asItem()
-				|| itemStack.getItem() == CarveMyPumpkin.CARVED_PUMPKIN.asItem()) {
+		if (itemStack.getItem() instanceof BlockItem
+				&& ((BlockItem) itemStack.getItem()).getBlock() instanceof CarvableBlock) {
 			ci.setReturnValue(false);
 		}
 	}
